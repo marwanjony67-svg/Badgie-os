5e5839123dcb67c5bd6a18892efcf4c9727752c9
marwanjony67-svg <marwanjony67@gmail.com>
2026-08-29 20:31:44 +0300

    Create keyboard.pp

---
+++ keyboard.pp
@@ -0,0 +1,12 @@
+#ifndef KEYBOARD_HPP
+#define KEYBOARD_HPP
+
+class Keyboard {
+public:
+    static char readChar() {
+        // Simplified port read simulation
+        return '1';
+    }
+};
+
+#endif
